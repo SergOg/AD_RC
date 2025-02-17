e1ef98dfac69d4cec7e8bfa082bf8d9dd8b78b7a
Sergey Ogurtsov <sergey.ogurtsov@gmail.com>
2025-02-17 14:30:23 +0300

    Удалено из comment_main <include layout="@layout/fragment_main" />, скорректирован mobile_navigation

--- app/src/main/java/ru/gb/rc/MainFragment.kt
+++ app/src/main/java/ru/gb/rc/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import ru.gb.rc.databinding.FragmentMainBinding
 import kotlin.random.Random
 
@@ -47,10 +49,15 @@ class MainFragment : Fragment() {
         binding.add.setOnClickListener {
             val item = Random.nextInt(100, 200).toString()
             myAdapter.addItem(5, item)
+            Toast.makeText(context, "Pressed add button!", Toast.LENGTH_SHORT).show()
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null)
+//                .setAnchorView(R.id.fab).show()
         }
 
         binding.remove.setOnClickListener {
             myAdapter.removeItem(1)
+            Toast.makeText(context, "Pressed remove button!", Toast.LENGTH_SHORT).show()
         }
 
 //        binding.set.setOnClickListener {
