191861d5a025730ed2a2d716c28f0aee992db457
Sergey Ogurtsov <sergey.ogurtsov@gmail.com>
2025-03-17 10:47:59 +0300

    Выводим название пульта

--- app/src/main/java/ru/gb/rc/DevicePultViewModel.kt
+++ app/src/main/java/ru/gb/rc/DevicePultViewModel.kt
@@ -1,7 +1,15 @@
 package ru.gb.rc
 
 import androidx.lifecycle.ViewModel
+import dagger.assisted.Assisted
+import ru.gb.rc.data.Device
+import ru.gb.rc.data.DeviceDao
 
-class DevicePultViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class DevicePultViewModel(
+    private val deviceDao: DeviceDao,
+    @Assisted val id: Int
+) : ViewModel() {
+    suspend fun pultView(device: Device){
+        val device = deviceDao.getOne(id)
+    }
 }
