a7bda1db78bd404707091934afe022bf0dec61fe
Sergey Ogurtsov <sergey.ogurtsov@gmail.com>
2025-03-26 21:10:44 +0300

    Добавляем работу с Bluetooth

--- app/src/main/java/ru/gb/rc/BluetoothFragment.kt
+++ app/src/main/java/ru/gb/rc/BluetoothFragment.kt
@@ -1,30 +1,37 @@
 package ru.gb.rc
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import ru.gb.rc.databinding.FragmentBluetoothBinding
+import ru.gb.rc.presentation.pult_device.DevicePultViewModel
 
 class BluetoothFragment : Fragment() {
+
+    private var _binding: FragmentBluetoothBinding? = null
+    private val binding get() = _binding!!
+    private val bluetoothViewModel: BluetoothViewModel by viewModels()
 
     companion object {
         fun newInstance() = BluetoothFragment()
     }
 
-    private val viewModel: BluetoothViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_bluetooth, container, false)
+        _binding = FragmentBluetoothBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDetach() {
+        super.onDetach()
     }
 }
--- app/src/main/java/ru/gb/rc/BluetoothViewModel.kt
+++ app/src/main/java/ru/gb/rc/BluetoothViewModel.kt
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import java.io.IOException
+import java.io.OutputStream
 import java.util.UUID
 
 class BluetoothViewModel(private val context: Context) : ViewModel() {
@@ -19,6 +20,7 @@ class BluetoothViewModel(private val context: Context) : ViewModel() {
     private val packageManager: PackageManager = context.packageManager
     private var bluetoothAdapter: BluetoothAdapter? = null
     private var socket: BluetoothSocket? = null
+    private lateinit var outputStream: OutputStream
 
     companion object {
         const val REQUEST_ENABLE_BT = 1
@@ -30,7 +32,7 @@ class BluetoothViewModel(private val context: Context) : ViewModel() {
         if (packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
             // Устройство поддерживает Bluetooth
             val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-            val bluetoothAdapter = bluetoothManager.adapter
+            bluetoothAdapter = bluetoothManager.adapter
         } else {
             // Устройства не поддерживает Bluetooth
             Toast.makeText(context, "Ваше устройство не поддерживает Bluetooth", Toast.LENGTH_SHORT)
@@ -51,12 +53,15 @@ class BluetoothViewModel(private val context: Context) : ViewModel() {
         bluetoothAdapter!!.startDiscovery()
     }
 
+        // Найти устройство по адресу
+    //        val device = bluetoothAdapter?.getRemoteDevice(deviceAddress)
     @SuppressLint("MissingPermission")
     fun connectToDevice(device: BluetoothDevice) {
         try {
             val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
             socket = device.createRfcommSocketToServiceRecord(uuid)
             socket?.connect()
+            outputStream = socket!!.outputStream
         } catch (e: IOException) {
             e.printStackTrace()
         }
